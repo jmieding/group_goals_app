8f3d46f3db39b0b4d7e1978fa39a5bb16d07edfd
JMieding <jtmieding@gmail.com>
2016-06-07 22:44:48 -0500

    add hidden row dropdown functionality

--- to_do/static/script.js
+++ to_do/static/script.js
@@ -1,11 +1,26 @@
-$(document).ready() 
-  $("td a div").click(function() {
-    if ($(this).children().length < 1) {
-      $(this).append("<span class='fa fa-check-circle fa-lg'></span>");
-      $(this).addClass("selected");
-    } else {
-      $(this).children().remove();
-      $(this).removeClass("selected");
+$(document).ready(function() {
+
+  $('i.fa-chevron-down').click(function() {
+    $(this).toggleClass('rotate');
+    var nextGoal = $(this).closest('tr').next();
+    while (true) {
+      if ($(nextGoal).hasClass('user')) {
+        break;
+      } else {
+        $(nextGoal).toggleClass('nested-goal');
+        $(nextGoal) = nextGoal.next();
+      }
     }
   });
-  // $.post( "/", { name: "John", time: "2pm" } );
+  
+  $('td a div').click(function() {
+    if ($(this).children().length < 1) {
+      $(this).append('<span class="fa fa-check-circle fa-lg"></span>');
+      $(this).addClass('selected');
+    } else {
+      $(this).children().remove();
+      $(this).removeClass('selected');
+    }
+  });
+  
+});
